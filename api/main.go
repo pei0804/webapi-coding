5dae1c3af41db9eb08a4ec41ddfa0212d2184068
pei0804 <peeeei0804@gmail.com>
2018-04-29 14:12:29 +0900

    2

--- api/main.go
+++ api/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
-// hello world, the web server
+// Account アカウント
+type Account struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+// HelloServer the web server
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "hello, world!\n")
+	account := Account{
+		ID:   1,
+		Name: "アカウント1",
+	}
+	res, err := json.Marshal(account)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
 func main() {
